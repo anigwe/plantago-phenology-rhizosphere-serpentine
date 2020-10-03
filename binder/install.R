7084562b314d2d16b30d66ec620c87ca77a2c9d6
anigwe <7919212+anigwe@users.noreply.github.com>
2020-10-03 15:00:23 -0700

    Update install.R

--- binder/install.R
+++ binder/install.R
@@ -1,15 +1,3 @@
-pkgs = c(
-"phyloseq",
-"dplyr",
-"ggpubr",
-"vegan",
-"ggplot2",
-"lubridate",
-"lme4",
-"survival",
-"survminer",
-"DESeq2",
-"car",
-"emmeans")
+pkgs = c("phyloseq","dplyr","ggpubr","vegan","ggplot2","lubridate","lme4","survival","survminer","DESeq2","car","emmeans","viridis")
 ncores = parallel::detectCores()
 install.packages(pkgs, Ncpus = ncores)
