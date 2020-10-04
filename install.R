ac2349af5c58590c7de0b295838f20792256f524
anigwe <7919212+anigwe@users.noreply.github.com>
2020-10-03 18:43:44 -0700

    Add files via upload

---
+++ install.R
@@ -0,0 +1,16 @@
+pkgs = c(
+"phyloseq",
+"dplyr",
+"ggpubr",
+"vegan",
+"ggplot2",
+"lubridate",
+"lme4",
+"survival",
+"survminer",
+"DESeq2",
+"car",
+"emmeans",
+"viridis")
+ncores = parallel::detectCores()
+install.packages(pkgs, Ncpus = ncores)
